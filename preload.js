09887352241b456c16ff62647f3a3f7a8e20546c
kts of kettek <kettek1@kettek.net>
2018-04-28 16:26:01 -0700

    Move element src/href rewriting to Render Pack

--- preload.js
+++ preload.js
@@ -37,26 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ipcRenderer.on('render', (event, message) => {
     window.ome.emit('render', message);
-    // Iterate through all elements with src and href, changing their src/href to point to the absolute directory relative to the file's path.
-    // NOTE: This seems very inefficient and silly. It would be nicer to implement a higher-level request proxy system, if possible.
-    let src_list = document.body.querySelectorAll('[src],[href]');
-    var r = new RegExp('^(?:[a-z]+:)?//', 'i');
-    for (let i = 0; i < src_list.length; ++i) {
-      let item = src_list[i];
-      // TODO: Many MD->HTML systems use a path relative to the markdown file ("page.md" -> "image.png"), not ("page.md" -> "page/image.png"), so there should be a configuration flag for that.
-      if (item.hasAttribute('href')) {
-        let href = item.getAttribute('href');
-        if (r.test(href) == false) {
-          item.setAttribute('href', path.join(path.dirname(filepath), path.basename(filepath, path.extname(filepath)), href));
-        }
-      }
-      if (item.hasAttribute('src')) {
-        let src = item.getAttribute('src');
-        if (r.test(src) == false) {
-          item.setAttribute('src', path.join(path.dirname(filepath), path.basename(filepath, path.extname(filepath)), src));
-        }
-      }
-    }
   });
   ipcRenderer.on('line', (event, message) => {
     window.ome.emit('line', message);
