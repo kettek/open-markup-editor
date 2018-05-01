3de669a2948d779db7aa1bffc3c9e8146f1a5d92
kts of kettek (Gliese) <kettek1@kettek.net>
2018-04-30 18:05:07 -0700

    Add M+ FONTS, specifically M+ C Type-2
    These are nice.

--- src/views/Tab.js
+++ src/views/Tab.js
@@ -8,7 +8,8 @@ module.exports = {
       { onclick: () => { Files.setFileFocus(vnode.attrs.fileIndex); } }
       ,
       [
-        m('span', Files.getFileName(vnode.attrs.fileIndex) + (!Files.isFileSaved(vnode.attrs.fileIndex) ? '*' : ''))
+        //m('span', Files.getFileName(vnode.attrs.fileIndex) + (!Files.isFileSaved(vnode.attrs.fileIndex) ? '*' : ''))
+        m('span'+(!Files.isFileSaved(vnode.attrs.fileIndex) ? '.unsaved' : ''), Files.getFileName(vnode.attrs.fileIndex))
       , m('button.close', { onclick: () => {
           Files.closeFile(vnode.attrs.fileIndex);
         }}, '' )
