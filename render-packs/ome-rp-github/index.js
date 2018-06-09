d9d0093230764b79ad7e0fd2cb02ae401e8aea9f
kts of kettek <kettek1@kettek.net>
2018-06-08 20:59:34 -0700

    Add GitHub-style render pack
    Perhaps this should be default!

---
+++ render-packs/ome-rp-github/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'GitHub',
+  targets: [".*"],
+  preload: __dirname+'/preload.js',
+  preview: __dirname+'/preview.html'
+};
---
+++ render-packs/ome-rp-github/preload.js
@@ -0,0 +1,37 @@
+const path = require('path');
+let target_element = null;
+let filepath = '';
+
+ome.on('filepath', (path) => {
+  filepath = path;
+});
+ome.on('ready', () => {
+  target_element = document.getElementById('OME_TARGET');
+});
+ome.on('render', html => {
+  target_element.innerHTML = html;
+  // Iterate through all elements with src and href, changing their src/href to point to the absolute directory relative to the file's path.
+  // NOTE: This seems very inefficient and silly. It would be nicer to implement a higher-level request proxy system, if possible.
+  let src_list = target_element.querySelectorAll('[src],[href]');
+  var r = new RegExp('^(?:[a-z]+:)?//', 'i');
+  for (let i = 0; i < src_list.length; ++i) {
+    let item = src_list[i];
+    if (item.hasAttribute('href')) {
+      let href = item.getAttribute('href');
+      if (r.test(href) == false) {
+        item.setAttribute('href', path.join(path.dirname(filepath),  href));
+      }
+    }
+    if (item.hasAttribute('src')) {
+      let src = item.getAttribute('src');
+      if (r.test(src) == false) {
+        item.setAttribute('src', path.join(path.dirname(filepath), src));
+      }
+    }
+  }
+
+});
+ome.on('line', line => {
+  let el = document.querySelector('[data-source-line="'+line+'"]');
+  if (el) el.scrollIntoView();
+});
