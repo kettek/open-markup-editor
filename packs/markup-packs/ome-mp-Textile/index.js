6aecb5ad0e507e6e40d476a0a5829949e257d116
kts of kettek (Tyger) <kettek1@kettek.net>
2019-01-27 02:15:58 -0800

    Implement Textile markup pack
    Closes #3

---
+++ packs/markup-packs/ome-mp-Textile/index.js
@@ -0,0 +1,44 @@
+module.exports = {
+  name: 'Textile',
+  supports: ['textile', 'txt'],
+  setup: pack => {
+    let textile = null;
+    let options = {};
+
+    pack.conf({
+      breaks: true,
+    },
+    ['section', {title: "Options for textile-js"},
+      ['checkbox', '', 'breaks'],
+      ['label', 'Line-break single newlines within blocks', 'breaks'],
+    ])
+
+    pack.render = (text) => {
+      return textile(text, options);
+    };
+
+    pack.cheatsheet = () => {
+      let cheatsheet = '';
+      return {
+        text: cheatsheet,
+        html: pack.render(cheatsheet)
+      }
+    };
+
+    pack.on('enable', () => {
+      textile = require('textile-js');
+      options['breaks'] = pack.get('breaks');
+    });
+
+    pack.on('conf-set', (key, value) => {
+      if (key == 'breaks') {
+        options[key] = value;
+      }
+    });
+    pack.on('disable', () => {
+      delete require.cache[require.resolve('textile-js')];
+      options = {};
+    });
+
+  }
+};
